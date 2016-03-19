3b3e16096e10d2c5617332432fbeb7d77708efc2
Les Pounder <lespounder@gmail.com>
2016-03-19 17:02:02 +0000

    Tested and working

---
+++ Robot Orchestra/2 - Ultrasonic Theremin/Sources.md
@@ -0,0 +1,5 @@
+#Project 2 - Ultrasonic Theremin
+
+The main source of this project is the gret work undertakenby gkvoelki who has found a way to interface Python with Sonic Pi.
+
+His original work is [here](https://github.com/gkvoelkl/python-sonic) and well worth a look.
--- Robot Orchestra/2 - Ultrasonic Theremin/ultrasonic.py
+++ Robot Orchestra/2 - Ultrasonic Theremin/ultrasonic.py
@@ -1,11 +1,12 @@
 import RPi.GPIO as GPIO
 import time
+from psonic import *
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-pinTrigger = 2
-pinEcho = 3
+pinTrigger = 17
+pinEcho = 18
 
 GPIO.setup(pinTrigger, GPIO.OUT)
 GPIO.setup(pinEcho, GPIO.IN)
@@ -14,18 +15,32 @@ global Distance
 def ultra():
     
     GPIO.output(pinTrigger, False)
-    time.sleep(0.5)
+
+    time.sleep(0.1)
+
     GPIO.output(pinTrigger, True)
     time.sleep(0.00001)
     GPIO.output(pinTrigger, False)
+
     StartTime = time.time()
+
     while GPIO.input(pinEcho)==0:
         StartTime = time.time()
     while GPIO.input(pinEcho)==1:
         StopTime = time.time()
+    
     ElapsedTime = StopTime - StartTime
     Distance = ElapsedTime * 34326
     Distance = Distance / 2
-    
+    print("Distance : %.1f" % Distance)
+    note = Distance /2
+    use_synth(MOD_PULSE)
+    play(note)
+    time.sleep(0.1)
+
+   
 while True:
+    
+    ultra()
+    
     
