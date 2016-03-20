06dc690749cafc4840915daaddc6668c4f94ca41
Les Pounder <lespounder@gmail.com>
2016-03-20 17:43:16 +0000

    Updating

--- Robot Orchestra/4 - Temperature Control/Temperature-Control.py
+++ Robot Orchestra/4 - Temperature Control/Temperature-Control.py
@@ -1,6 +1,5 @@
 from sense_hat import SenseHat
 import pygame.mixer
-
 import time
 
 pygame.mixer.init()
@@ -10,7 +9,7 @@ def audio(music):
     
 
 sense = SenseHat()
-#sense.show_message("Ready to party!!", text_colour=[255,255,0])
+sense.show_message("Ready to party!!", text_colour=[255,255,0])
 cold = "./music/loop_amen.wav"
 hot = "./music/loop_industrial.wav"
 while True:
@@ -20,7 +19,7 @@ while True:
     if round(temperature) < 33:
         audio(cold)
         sense.show_message("COLD", scroll_speed=0.03,text_colour=[0,0,255])
-    elif round(temperature) == 33:
+    elif round(temperature) >= 33:
         audio(hot)
         sense.show_message("HOT", scroll_speed=0.03, text_colour=[255,0,0])
         
