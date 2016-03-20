d2f1d266ef6c9141e939fe06090f3346e78569e5
Les Pounder <lespounder@gmail.com>
2016-03-20 17:46:24 +0000

    Moved work to right folder

---
+++ Robot Orchestra/3 - Motor Percussion/percussion.py
@@ -0,0 +1,17 @@
+from gpiozero import Motor
+import time
+motor1 = Motor(9,10)
+motor2 = Motor(7,8)
+
+while True:
+    
+    for i in range(2):
+        motor1.backward(speed=0.3)
+        motor2.forward(speed=0.9)
+        time.sleep(1)
+    for i in range(2):
+        motor1.backward(speed=1)
+        motor2.forward(speed=0.2)
+        time.sleep(1)    
+    motor1.stop()
+    motor2.stop()
